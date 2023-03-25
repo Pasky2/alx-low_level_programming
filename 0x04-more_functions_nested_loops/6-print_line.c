650b3ab78e2db56dc98df3d3da5cdd031857f194
Pasky2 <pascalosemene@gmail.com>
2023-03-24 17:34:12 -0700

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_line -  function that draws a straight line in the terminal
+ * @n: Parameter to be checked
+ */
+void print_line(int n)
+{
+for (; n > 0; n--)
+_putchar(' ');
+_putchar('\n');
+}
