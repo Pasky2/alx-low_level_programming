0e96bbf84689e82affe5925c8f78bfb2084c38ce
Pasky2 <pascalosemene@gmail.com>
2023-04-03 23:47:12 -0700

    Task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _memset - Function that fills memory with a constant byte
+ * @s: memory to be filled
+ * @b: parameter to copy
+ * @n: number of times to copy
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int j;
+
+	for(j = 0; j < n; j++)
+	{
+		s[j] = b;
+	}
+
+	return (s);
+}
